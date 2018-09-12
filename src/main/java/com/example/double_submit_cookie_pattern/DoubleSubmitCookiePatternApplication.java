afe35ad733b4d59510285c3715e54f8c7171d1df
madupoorna <karunadhipathis@gmail.com>
2018-09-12 22:02:35 +0530

    first commit

---
+++ src/main/java/com/example/double_submit_cookie_pattern/DoubleSubmitCookiePatternApplication.java
@@ -0,0 +1,12 @@
+package com.example.double_submit_cookie_pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubleSubmitCookiePatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoubleSubmitCookiePatternApplication.class, args);
+	}
+}
